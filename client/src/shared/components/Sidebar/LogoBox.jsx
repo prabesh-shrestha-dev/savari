8b8cf926bd5f8064e33393ed96268b003e7ab7c6
Prabesh Shrestha <prabeshshrestha0112@gmail.com>
2026-08-10 00:03:33 +0545

    Final Version 1.1

--- client/src/shared/components/Sidebar/LogoBox.jsx
+++ client/src/shared/components/Sidebar/LogoBox.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import logo from "../../../assets/876.png";
+import logo from "../../../assets/logo.png";
 import "./LogoBox.css";
 import { ShieldCheck } from "lucide-react";
 
